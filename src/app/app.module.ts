b19a6130fbc70ecfcbcc0225d384208da6cd66df
Bartłomiej Kot <bartlomiej.kot@microsoft.wsei.edu.pl>
2022-03-23 20:58:00 +0100

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,19 @@ import { MainPageComponent } from './Components/main-page/main-page.component';
 import { LeftMenuComponent } from './Components/left-menu/left-menu.component';
 import { TopBarComponent } from './Components/top-bar/top-bar.component';
 import { LoginPageComponent } from './Components/login-page/login-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import {MatIconModule} from '@angular/material/icon';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +29,17 @@ import { LoginPageComponent } from './Components/login-page/login-page.component
     LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
